b6a6d4027050b5f2cb9f70f7095f2630386aefaf
Gogo602 <gogomarosi61@gmail.com>
2025-04-11 22:41:36 +0300

    fixed props issue

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -47,7 +47,7 @@ export async function POST(request: NextRequest) {
         })
         return response;
         
-    } catch (error: any) {
+    } catch (error) {
         return NextResponse.json({ error: error.message }, 
             { status: 500 }
         )
