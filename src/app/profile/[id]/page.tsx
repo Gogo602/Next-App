22e52f03e93395f2354d745f8744c29fe2658da5
Gogo602 <gogomarosi61@gmail.com>
2025-04-11 05:50:18 +0300

    fixed props issue

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,13 +1,21 @@
+import type { NextPage } from 'next';
 
-
-export default function UserProfile({ params }: { params: { id: string } }) {
-    return (
-        <div className="flex items-center justify-center h-screen">
-            <div>
-                <h1>Profile</h1>
-                <hr />
-                <p className='text-2xl'>Profile Page {params.id}</p>
-            </div>
-        </div>
-    )
+interface Props {
+  params: {
+    id: string;
+  };
 }
+
+const UserProfile: NextPage<Props> = ({ params }) => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div>
+        <h1>Profile</h1>
+        <hr />
+        <p className='text-2xl'>Profile Page {params.id}</p>
+      </div>
+    </div>
+  );
+};
+
+export default UserProfile;
